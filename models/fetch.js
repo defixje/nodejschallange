606d4123dead0ba9d1e16457bec965956e44ea5c
edirks <e.dirks@onsweb.nl>
2017-05-11 11:30:49 +0200

    :racehorse:  just one open connection

--- models/fetch.js
+++ models/fetch.js
@@ -3,20 +3,21 @@ const Promise = require('bluebird');
 const redis = require('../classes/redis');
 const mysql = require('../classes/mysql');
 const push = require('./push');
+const sourcedb = mysql.sourcedb();
+const destinationdb = mysql.destinationdb();
 
 module.exports = {
   goindex: function () {
     this.start()
       .then(() => {
-        console.log('Leggo');
+        //console.log('Leggo');
         this.goindex()
       });
   },
   start: function () {
     return new Promise((resolve, reject) => {
-      const destinationdb = mysql.destinationdb();
 
-      mysql.getRowsFromSource(15000, (err, data) => {
+      mysql.getRowsFromSource(sourcedb, 15000, (err, data) => {
           return Promise.resolve(data).map(item => {
 
             var jsonData = JSON.stringify(item);
@@ -24,7 +25,7 @@ module.exports = {
 
             return redis.checkhash(hashed)
               .then(reply => {
-                // console.log('Already:', hashed);
+                //console.log('Already up-2-date', hashed);
               })
               .catch(err => {
                 if (err.message === 'not_found') {
@@ -36,8 +37,11 @@ module.exports = {
                 return Promise.reject(err);
               });
 
+
           }, {concurrency: 100})
-            .then(() => resolve())
+            .then(() => {
+              resolve()
+            })
             .catch(err => {
               console.log(err)
             }
